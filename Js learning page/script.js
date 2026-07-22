d6906801e765b6a207bceb40693f1fc3fd26a944
RollinNoodle <mark.groblar.m@gmail.com>
2026-07-22 16:02:57 +0200

    First commit

---
+++ Js learning page/script.js
@@ -0,0 +1,31 @@
+// counter "engine"
+
+const theAdd = document.getElementById("addBtn");
+const theReset = document.getElementById("resetBtn");
+const theTake = document.getElementById("takeBtn");
+const theDisplay = document.getElementById("displayBtn");
+
+count = 0;
+
+theAdd.addEventListener("click", () => {
+    count++
+    theDisplay.textContent = count;
+});
+
+theTake.addEventListener("click", () => {
+    count--;
+    theDisplay.textContent = count;
+});
+
+theReset.addEventListener("click", () => {
+    count = 0;
+    theDisplay.textContent = count;
+});
+
+// dark mode:
+
+const switchMode = document.getElementById("dark-mode");
+
+switchMode.addEventListener("click", () => {
+    document.body.classList.toggle("darkMode");
+});
